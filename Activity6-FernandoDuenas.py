8374919f535c94866764fb3836c2effa0d18eb79
Fernando Duenas <fernandoduenas@Fernandos-MBP.lan>
2021-09-15 16:01:41 -0400

    activity6

---
+++ Activity6-FernandoDuenas.py
@@ -0,0 +1,22 @@
+'''
+Activity 6: Dictionaries
+* Create an activity file.
+'''
+# In this file I want you to create a Dictionary named "car".
+# The car has a brand, a model, and year.
+
+car = {
+    "brand": "ford",
+    "model": "mustang",
+    "year": "1964"
+}
+
+print(car['model'])
+
+'''
+Perform Dictionary Methods on your Object, and in a comment explain the result you expected vs the result that you got.
+'''
+
+# i expect to get an empty Dictionany.
+print(car.clear())
+print(car)
