c8b2674e302db94f4717b7caf1636d11c1f201e5
Fernando Duenas <fernandoduenas@Fernandos-MBP.lan>
2021-09-16 14:26:25 -0400

    activity 7

---
+++ Activity7-FernandoDuenas.py
@@ -0,0 +1,19 @@
+# Create a function named add that expects two arguments
+
+#Return the sum of those two arguments.
+
+#Repeat this process for Subtracting, Multiplying and Dividing.
+
+def add(one, two):
+    return one + two
+
+def sub(one, two):
+    return one - two
+    
+def mult(one, two):
+    return one * two
+
+def div(one, two):
+    return one / two
+
+print(div(6,2))
