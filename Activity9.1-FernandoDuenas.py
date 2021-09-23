eb5bc10d9b641215daf1bb6467e3e40c802e922d
Fernando Duenas <fernandoduenas@Fernandos-MBP.lan>
2021-09-23 12:52:04 -0400

    act9.1 with comments

--- Activity9.1-FernandoDuenas.py
+++ Activity9.1-FernandoDuenas.py
@@ -4,19 +4,19 @@
 
 '''
 
-numbers = [8, 3, 5, 9, 1]
-lenNums = len(numbers)
-newList = []
-def difNums(nums):
-    i = 0
+numbers = [8, 3, 5, 9, 1]           # Hardcoded list
+lenNums = len(numbers)              # assigning the list items length to a variable
+newList = []                        # creating an empty List to save the sorted List
+def difNums(nums):              
+    i = 0                           # setting initial value as 0
 
-    while(i < lenNums):
-        minVal = min(numbers)
-        newList.append(minVal)
-        numbers.remove(minVal)
-        i += 1
+    while(i < lenNums):             
+        minVal = min(numbers)       # finding the minimun value in the "numbers List"
+        newList.append(minVal)      # appending that value to the "newList"
+        numbers.remove(minVal)      # removing that value from the original List
+        i += 1                      # increment
 
-    print(newList)
+    print(newList)                  # display the newList on the CLI
  
 
-difNums(numbers)
+difNums(numbers)                    # passing the "numbers List" through the function given
